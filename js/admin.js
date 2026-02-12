accbb16d6f71a2534c3aad200545f10589c49d0e
redarbib <6043924@mborijnland.nl>
2026-02-12 11:02:50 +0100

    V0.1.6.2

--- js/admin.js
+++ js/admin.js
@@ -14,3 +14,20 @@ function Tabs(evt, cityName) {
 	document.getElementById(cityName).style.display = "block";
 	evt.currentTarget.className += " active";
 }
+
+function PackageTabs(evt, tabName) {
+	var i, subtabcontent, subtablinks;
+
+	subtabcontent = document.getElementsByClassName("packages-subtabcontent");
+	for (i = 0; i < subtabcontent.length; i++) {
+		subtabcontent[i].style.display = "none";
+	}
+
+	subtablinks = document.getElementsByClassName("packages-subtablinks");
+	for (i = 0; i < subtablinks.length; i++) {
+		subtablinks[i].className = subtablinks[i].className.replace(" active", "");
+	}
+
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
